a4ced0b076468112f4e1fb87acba38c1b841d5f3
LeonArchie <levmikhailovish@yandex.ru>
2025-08-15 17:51:27 +0500

    update deployments and app

--- app/app.py
+++ app/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def health():
     return jsonify({"status": True}), 200
 
+@app.route('/healthz', methods=['GET'])
+def healthz():
+    return jsonify({"status": "healthy"}), 200
+
+@app.route('/readyz', methods=['GET'])
+def readyz():
+    return jsonify({"status": "ready"}), 200
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
